3579e63370316cde015eeb2d7b5b7991fed56991
unknown <23521354@gm.uit.edu.vn>
2025-11-20 13:18:45 +0700

    Initial backend commit

---
+++ app.js
@@ -0,0 +1,12 @@
+// Express app entry point
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+app.use(cors());
+app.use(bodyParser.json());
+
+// ...routes will be added here
+
+module.exports = app;
